876ce546a28511e0823294c28c5df4d5f0387b8a
rtogoev <rtogoev92@yandex.ru>
2023-02-14 20:31:41 +0300

    add mode 2

--- EconomyOfSingle/src/Main.java
+++ EconomyOfSingle/src/Main.java
@@ -1,9 +1,40 @@
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.println("mode 1 - money, days");
+        System.out.println("mode 2 - money, end-date");
+        System.out.println("mode 3 - 0000000000000");
+        System.out.println();
+        System.out.println("choose mode:");
+        switch (scanner.nextLine()) {
+            case "1" -> mode1(scanner);
+            case "2" -> mode2(scanner);
+            case "3" -> mode3(scanner);
+        }
+    }
+
+    private static void mode3(Scanner scanner) {
+
+    }
+
+    private static void mode2(Scanner scanner) {
+        System.out.println("input your money: ");
+        int money = scanner.nextInt();
+        System.out.println("input end date (yyyy-MM-dd): ");
+        LocalDate endDate = LocalDate.parse(scanner.next());
+        int daysToSurvive = Period.between(LocalDate.now(), endDate).getDays();
+        int moneyPerDay = money / daysToSurvive;
+        System.out.println("money per day: " + moneyPerDay);
+        System.out.println("Start day: " + LocalDate.now());
+        System.out.println("End day: " + LocalDate.now().plusDays(daysToSurvive));
+    }
+
+    public static void mode1(Scanner scanner) {
         System.out.println("input your money: ");
         int money = scanner.nextInt();
         System.out.println("input number of days to survive: ");
